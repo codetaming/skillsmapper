d2d6cb744d29a595e07fc40879f83ccf10d982f4
Daniel Vaughan <dan@danielvaughan.com>
2020-02-15 09:17:57 +0000

    mapped skills fields to json

--- internal/model/model.go
+++ internal/model/model.go
@@ -3,9 +3,9 @@ package model
 import "time"
 
 type Skill struct {
-	SkillID string
-	Created time.Time
-	Email   string
-	Tag     string
-	Level   string
+	SkillID string    `json:"skill_id"`
+	Created time.Time `json:"created"`
+	Email   string    `json:"email"`
+	Tag     string    `json:"tag"`
+	Level   string    `json:"level"`
 }
